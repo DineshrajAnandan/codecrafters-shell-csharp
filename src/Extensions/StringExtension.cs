35d14f2f31fccc0b7051e1f9d9729f397a2863e4
dineshraja <dineshraj.anandan@thoughtworks.com>
2025-10-01 22:27:22 +0530

    refactor

--- src/Extensions/StringExtension.cs
+++ src/Extensions/StringExtension.cs
@@ -4,13 +4,13 @@ public static class StringExtension
 {
     public static string TrimTrailingEmptyLine(this string input)
     {
-        if (!string.IsNullOrEmpty(input))
+        if (string.IsNullOrEmpty(input)) 
+            return input;
+        
+        var lines = input.Split('\n');
+        if (lines.Length > 0 && string.IsNullOrWhiteSpace(lines[^1]))
         {
-            var lines = input.Split('\n');
-            if (lines.Length > 0 && string.IsNullOrWhiteSpace(lines[^1]))
-            {
-                input = string.Join('\n', lines, 0, lines.Length - 1);
-            }
+            input = string.Join('\n', lines, 0, lines.Length - 1);
         }
         return input;
     }
