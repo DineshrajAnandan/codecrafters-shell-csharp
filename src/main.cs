2f6cb0ddb9f2112fae918c7b2a3d315e5dac3c3d
dineshraja <dineshraj.anandan@thoughtworks.com>
2025-10-01 13:02:43 +0530

    implement REPL

--- src/main.cs
+++ src/main.cs
@@ -1,6 +1,7 @@
-Console.Write("$ ");
+while (true)
+{
+    Console.Write("$ ");
+    var command = Console.ReadLine();
 
-// Wait for user input
-var command = Console.ReadLine();
-
-Console.WriteLine($"{command}: command not found");
+    Console.WriteLine($"{command}: command not found");
+}
