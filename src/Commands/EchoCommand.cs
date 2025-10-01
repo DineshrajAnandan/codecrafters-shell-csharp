4736853ba51f73fe0111ead403960b47384a2535
dineshraja <dineshraj.anandan@thoughtworks.com>
2025-10-01 21:57:42 +0530

    debug: echo change

--- src/Commands/EchoCommand.cs
+++ src/Commands/EchoCommand.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using CodecraftersShell.Extensions;
 
 namespace CodecraftersShell.Commands;
@@ -6,17 +7,24 @@ public class EchoCommand: ICommand
 {
     public void Handle(string arguments)
     {
-        var text = arguments.GetTextWithinSingleQuotes(out var hasQuotes);
-        if (hasQuotes)
-        {
-            Console.WriteLine(text);
-            return;
-        }
-
-        var textArray = text.Split(' ')
-            .Select(s => s.Trim())
-            .Where(s => !string.IsNullOrEmpty(s));
-        var output = string.Join(" ", textArray);
-        Console.WriteLine(output);
+        var texts = Regex.Matches(arguments, @"[^\s""']+|'([^']*)'")
+            .Cast<Match>()
+            .Select(m => m.Value.Trim('\''))
+            .ToArray();
+        
+        Console.WriteLine(string.Join(" ", texts));
+        
+        // var text = arguments.GetTextWithinSingleQuotes(out var hasQuotes);
+        // if (hasQuotes)
+        // {
+        //     Console.WriteLine(text);
+        //     return;
+        // }
+        //
+        // var textArray = text.Split(' ')
+        //     .Select(s => s.Trim())
+        //     .Where(s => !string.IsNullOrEmpty(s));
+        // var output = string.Join(" ", textArray);
+        // Console.WriteLine(output);
     }
 }
