b2e64687291613084ed7f6a6b3fb6abde51ae169
dineshraja <dineshraj.anandan@thoughtworks.com>
2025-10-03 20:24:24 +0530

    fix

--- src/CommandInput.cs
+++ src/CommandInput.cs
@@ -50,7 +50,7 @@ public class CommandInput
 
         if (!showAllCommands)
         {
-            Append(remainingSubString + " ");
+            Append(remainingSubString);
             return;
         }
         
